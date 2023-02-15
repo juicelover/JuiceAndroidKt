6ab6a8f15a89f0e83ddcb54f882d4e2cbd5e3fa7
15954853625 <dream666>
2023-02-15 10:01:47 +0800

    juice: 2023-01-16 08:44:19 新增  cameraX

---
+++ app/src/main/java/com/dream/juiceandroid/developers/codelab/activity/CameraxNewActivity.kt
@@ -0,0 +1,17 @@
+package com.dream.juiceandroid.developers.codelab.activity
+
+import com.dream.juiceandroid.R
+import com.dream.juiceandroid.databinding.ActivityCameraxNewBinding
+import com.juiceandroid.base_lib.activity.BaseActivity
+
+class CameraxNewActivity :BaseActivity<ActivityCameraxNewBinding>() {
+    override fun getLayoutId(): Int  = R.layout.activity_camerax_new
+
+    override fun initView() {
+
+    }
+
+    override fun loadData(isRefresh: Boolean) {
+
+    }
+}
